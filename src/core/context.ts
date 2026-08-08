e3911185318da89bc3f4f7d704a00c96c3c054bf
Yusef Codes <youcefcopy@gmail.com>
2026-08-08 15:34:42 +0100

    ClaireContext updated: ClaireRequest & ClaireResponse composition and setup

--- src/core/context.ts
+++ src/core/context.ts
@@ -1,3 +1,13 @@
+import { ClaireRequest } from "./request";
+import { ClaireResponse } from "./response";
+
 export class ClaireContext {
     // composition of both ClaireRouter & ClaireResponse here
+    public request: ClaireRequest;
+    public response: ClaireResponse;
+
+    constructor (req: Request) {
+        this.request = new ClaireRequest(req);
+        this.response = new ClaireResponse();
+    }
 }
