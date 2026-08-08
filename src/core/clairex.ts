651f513296f6b29f7b7baab7c9388712785881b0
Yusef Codes <youcefcopy@gmail.com>
2026-08-08 16:37:51 +0100

    ClaireX updated: checking if the parrams match and passing the context to the handler so the user works with it

--- src/core/clairex.ts
+++ src/core/clairex.ts
@@ -1,12 +1,12 @@
 import { ClaireRouter } from "./router";
 import { ClaireContext } from "./context";
-export class ClaireX extends ClaireRouter  {
-    private port;
+export class ClaireX extends ClaireRouter {
+  private port;
 
-    constructor (port?: number) {
-        super();
-        this.port = port ?? 3000;
-    }
+  constructor(port?: number) {
+    super();
+    this.port = port ?? 3000;
+  }
 
   listen() {
     Bun.serve({
@@ -16,11 +16,12 @@ export class ClaireX extends ClaireRouter  {
         const context = new ClaireContext(req);
 
         for (const route of this.routes) {
-            if (route.method !== context.request.method) continue; //skip to next iteration
-            return route.handler;
-        }
+          if (route.method !== context.request.method) continue; //skip to next iteration
+          if (route.pattern !== context.request.pathname) continue;
 
-      }
+          return route.handler(context);
+        }
+      },
     });
 
     console.log(`ClaireX running on ${this.port}`);
