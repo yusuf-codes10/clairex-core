b8af61e3dff923012e7fd19ee6362593ac3b1f42
Yusef Codes <youcefcopy@gmail.com>
2026-08-08 16:06:01 +0100

    ClaireRequest updated: providing getters for encaspsulation sake | backing field pattern

--- src/core/request.ts
+++ src/core/request.ts
@@ -1,14 +1,14 @@
 export class ClaireRequest {
     private raw: Request;
     public params: Record<string, string>;
-    private method: string;
-    private url: URL;
+    private _method: string;
+    private _url: URL;
 
     constructor (req: Request, params: Record<string, string> = {}) {
         this.raw = req;
         this.params = params;
-        this.url = new URL(req.url);
-        this.method = req.method;
+        this._url = new URL(req.url);
+        this._method = req.method;
     }
 
     async json () {
@@ -17,5 +17,13 @@ export class ClaireRequest {
 
     async text () {
         return await this.raw.text();
+    }
+
+    get method () {
+        return this._method;
+    }
+
+    get url () {
+        return this._url;
     }
 }
