03917d401a4481172df87e409db550ddea92af9f
Yusef Codes <youcefcopy@gmail.com>
2026-08-09 17:06:18 +0100

    ClaireController added: setting up its fields and constructor

---
+++ src/core/controller.ts
@@ -0,0 +1,10 @@
+import { ClaireRouter } from "./router";
+
+export abstract class ClaireController {
+    protected _router = new ClaireRouter();
+    protected prefix: string;
+
+    constructor (prefix: string) {
+        this.prefix = prefix;
+    }
+}
