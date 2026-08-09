512fec4d32c017ac2ed175a4f0b3bf65efb3c5c2
Yusef Codes <youcefcopy@gmail.com>
2026-08-09 13:05:51 +0100

    ClaireResponse updated: Encapsulation forced and a private status

--- src/core/response.ts
+++ src/core/response.ts
@@ -1,41 +1,45 @@
 export class ClaireResponse {
-  private status: number;
+  private _status: number;
 
   constructor(status: number = 200) {
-    this.status = status;
+    this._status = status;
   }
 
   json(data: unknown, status: number = 200) {
-    this.status = status;
+    this._status = status;
     return new Response(JSON.stringify(data), {
-      status: this.status,
+      status: this._status,
       headers: { "Content-Type": "application/json" },
     });
   }
 
   text(data: string, status: number = 200) {
-    this.status = status;
+    this._status = status;
     return new Response(data, {
-      status: this.status,
+      status: this._status,
       headers: { "Content-Type": "text/plain" },
     });
   }
 
   html(data: string, status: number = 200) {
-    this.status = status;
+    this._status = status;
     return new Response(data, {
-      status: this.status,
+      status: this._status,
       headers: { "Content-Type": "text/html" },
     });
   }
 
   redirect(url: string, status: 301 | 302 = 302) {
-    this.status = status;
+    this._status = status;
     return new Response(null, {
-      status: this.status,
+      status: this._status,
       headers: {
         Location: url,
       },
     });
+  }
+
+  get status () {
+    return this._status;
   }
 }
