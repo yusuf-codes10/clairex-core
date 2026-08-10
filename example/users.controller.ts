bd72924260d62daee4b691b50ce070e9859606b0
Yusef Codes <youcefcopy@gmail.com>
2026-08-10 12:23:40 +0100

    ClaireController tested: seems stable for now

--- example/users.controller.ts
+++ example/users.controller.ts
@@ -1,7 +1,12 @@
 import { ClaireController } from "../src/core/controller";
 import { ClaireContext } from "../src/core/context";
 
-const users = [
+type User = {
+    name: string,
+    age: number
+}
+
+const users: User[] = [
   { name: "Claire", age: 23 },
   { name: "John", age: 33 },
   { name: "Veronica", age: 28 },
@@ -15,9 +20,16 @@ export class userController extends ClaireController {
 
     register () {
         this.routes('get', '/', this.getUsers);
+        this.routes('post', '/', this.createUser);
     }
 
     private getUsers (c: ClaireContext) {
+        return c.response.json(users);
+    }
+
+    private async createUser  (c: ClaireContext) {
+        const body = (await c.request.json()) as {name: string, age: number};
+        users.push(body);
         return c.response.json(users);
     }
 }
