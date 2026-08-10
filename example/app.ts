21f5aa15f930ef0827b5826396dec7d511f70d7f
Yusef Codes <youcefcopy@gmail.com>
2026-08-10 17:44:58 +0100

    implementing the first ClaireMiddlware

--- example/app.ts
+++ example/app.ts
@@ -1,9 +1,12 @@
 import { ClaireX } from "../src";
 import { userController } from '../example/users.controller';
+import { logger } from "./logger";
 
 const app = new ClaireX(2300);
 
 // need a way to mount the extends class here
 app.mount(new userController());
+
+app.use(new logger());
 
 app.listen();
--- example/logger.ts
+++ example/logger.ts
@@ -1,7 +1,8 @@
+import type { ClaireContext } from "../src/core/context";
 import { ClaireMiddleware } from "../src/core/middleware";
 
 export class logger extends ClaireMiddleware {
-    override before () {
-        console.log('nothing!');
+    override before (c: ClaireContext) {
+        console.log('first middlware', c.request.method, c.request.pathname);
     }
 }
