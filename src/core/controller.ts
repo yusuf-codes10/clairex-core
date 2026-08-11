60b19b94856066c917a39be4adad33ee389b537f
Yusef Codes <youcefcopy@gmail.com>
2026-08-11 18:05:37 +0100

    ClaireController updated: pushing the route level middleware as weel

--- src/core/controller.ts
+++ src/core/controller.ts
@@ -3,33 +3,39 @@ import type { RouterEntry, ClaireHandler } from "./types";
 import { ClaireMiddleware } from "./middleware";
 
 export abstract class ClaireController {
-    protected _router = new ClaireRouter();
-    protected prefix: string;
+  protected _router = new ClaireRouter();
+  protected prefix: string;
 
-    // each ClaireController has its own middleware chain
-    private _middlewareChain?: ClaireMiddleware[] = [];
+  // each ClaireController has its own middleware chain
+  private _middlewareChain?: ClaireMiddleware[] = [];
 
-    constructor (prefix: string, middlewares: ClaireMiddleware[] = []) {
-        this._middlewareChain = middlewares;
-        this.prefix = prefix;
-        this.register() // have to call the register method after we have the prefix
-    }
+  constructor(prefix: string, middlewares: ClaireMiddleware[] = []) {
+    this._middlewareChain = middlewares;
+    this.prefix = prefix;
+    this.register(); // have to call the register method after we have the prefix
+  }
 
-    protected abstract register(): void;
+  protected abstract register(): void;
 
-    protected routes(
-        method: 'get' | 'post' | 'put' | 'patch' | 'delete',
-        path: string,
-        handler: ClaireHandler
-    ): void {
-        this._router[method](`${this.prefix}${path}`, handler.bind(this));
-    }
+  protected routes(
+    method: "get" | "post" | "put" | "patch" | "delete",
+    path: string,
+    handler: ClaireHandler,
+    middleware?: ClaireMiddleware[],
+  ): void {
+    this._router.routes.push({
+      method: method.toUpperCase(),
+      pattern: `${this.prefix}${path}`,
+      handler: handler.bind(this),
+      routeMiddlewares: middleware,
+    });
+  }
 
-    get router(): RouterEntry[] {
-        return this._router.routes;
-    }
+  get router(): RouterEntry[] {
+    return this._router.routes;
+  }
 
-    get middlewares(): ClaireMiddleware[] | undefined {
-        return this._middlewareChain;
-    }
+  get middlewares(): ClaireMiddleware[] | undefined {
+    return this._middlewareChain;
+  }
 }
