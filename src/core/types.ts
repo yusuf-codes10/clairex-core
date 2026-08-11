be11e8bce5afe0d4cd7f586950ce2907eb213712
Yusef Codes <youcefcopy@gmail.com>
2026-08-11 15:55:27 +0100

    types updated: an optional middlewares type in RouterEntry

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,11 +1,13 @@
 // types
 
-import type { ClaireContext } from "./context"
+import type { ClaireContext } from "./context";
+import { ClaireMiddleware } from "./middleware";
 
 export type ClaireHandler = (c: ClaireContext) => Response | Promise<Response>;
 
 export type RouterEntry = {
     method: string,
     pattern: string,
-    handler: ClaireHandler
+    handler: ClaireHandler,
+    middlewares?: ClaireMiddleware[]
 }
