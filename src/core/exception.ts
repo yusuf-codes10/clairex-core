1ef406d1e8fb690ea8da6e6c5d1f927b79d7cce5
Yusef Codes <youcefcopy@gmail.com>
2026-08-11 12:26:21 +0100

    ClaireException updated: basic fileds and getters

--- src/core/exception.ts
+++ src/core/exception.ts
@@ -1,3 +1,18 @@
 export class ClaireException extends Error {
-    
+    private _statusCode: number;
+    private _content: string;
+
+    constructor (statusCode: number, content: string) {
+        super();
+        this._statusCode = statusCode;
+        this._content = content;
+    }
+
+    get statusCode () {
+        return this._statusCode;
+    }
+
+    get content() {
+        return this._content;
+    }
 }
