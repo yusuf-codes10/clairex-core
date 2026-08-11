d109424082513e9823e82e9fbb2732c439b9d4e4
Yusef Codes <youcefcopy@gmail.com>
2026-08-11 12:31:41 +0100

    ClaireException updated: might come in handy metada optional fileds

--- src/core/exception.ts
+++ src/core/exception.ts
@@ -1,6 +1,7 @@
 export class ClaireException extends Error {
     private _statusCode: number;
     private _content: string;
+    private _metadata?: Record<string, string>; // might come in handy as debug info
 
     constructor (statusCode: number, content: string) {
         super();
@@ -14,5 +15,9 @@ export class ClaireException extends Error {
 
     get content() {
         return this._content;
+    }
+
+    get metadata () {
+        return this._metadata;
     }
 }
