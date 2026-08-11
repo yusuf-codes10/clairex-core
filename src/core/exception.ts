097cd0f72e3de4c789c522d35d38cdee7990e176
Yusef Codes <youcefcopy@gmail.com>
2026-08-11 13:08:17 +0100

    ClaireException updated: metada is set if exist

--- src/core/exception.ts
+++ src/core/exception.ts
@@ -3,11 +3,12 @@ export class ClaireException extends Error {
     private _content: string;
     private _metadata?: Record<string, string>; // might come in handy as debug info
 
-    constructor (statusCode: number, content: string) {
+    constructor (statusCode: number, content: string, metadata: Record<string, string>) {
         super(content);
         this._statusCode = statusCode;
         this._content = content;
         this.name = 'ClaireException';
+        this._metadata = metadata;
     }
 
     get statusCode(): number {
