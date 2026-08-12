e390ca79a50428cebb81f4d0b7db84eff6158960
Yusef Codes <youcefcopy@gmail.com>
2026-08-12 13:09:11 +0100

    ClaireLogger updated: using after to show the duration of the request

--- src/middleware/ClaireLogger.ts
+++ src/middleware/ClaireLogger.ts
@@ -2,7 +2,16 @@ import type { ClaireContext } from "../core/context";
 import { ClaireMiddleware } from "../core/middleware";
 
 export class ClaireLogger extends ClaireMiddleware {
+    private start: number = 0;
+
     override before(c: ClaireContext) {
-        console.log(`${c.request.method} ${c.request.url}`);
+        this.start = performance.now();
+        console.log(`→ ${c.request.method} ${c.request.url}`);
+    }
+
+    override after(c: ClaireContext, response: Response): Response {
+        const duration = (performance.now() - this.start).toFixed(2);
+        console.log(`← ${c.request.method} ${c.request.url} ${duration}ms`);
+        return response;
     }
 }
