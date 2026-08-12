337062ce1915f9d7b63b30e455969e7e16fbf035
Yusef Codes <youcefcopy@gmail.com>
2026-08-12 13:30:30 +0100

    ClaireLogger updated: using that colorMethod util

--- src/middleware/ClaireLogger.ts
+++ src/middleware/ClaireLogger.ts
@@ -1,17 +1,18 @@
 import type { ClaireContext } from "../core/context";
 import { ClaireMiddleware } from "../core/middleware";
+import { colorMethod } from "../core/utils";
 
 export class ClaireLogger extends ClaireMiddleware {
     private start: number = 0;
 
     override before(c: ClaireContext) {
         this.start = performance.now();
-        console.log(`→ ${c.request.method} ${c.request.url}`);
+        console.log(`→ ${colorMethod(c.request.method)} ${c.request.url}`);
     }
 
     override after(c: ClaireContext, response: Response): Response {
         const duration = (performance.now() - this.start).toFixed(2);
-        console.log(`← ${c.request.method} ${c.request.url} ${duration}ms`);
+        console.log(`← ${colorMethod(c.request.method)} ${c.request.url} ${duration}ms`);
         return response;
     }
 }
