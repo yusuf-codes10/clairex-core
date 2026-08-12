24301505ac8b8e845cfae6f16bed2e23fe84b3ce
Yusef Codes <youcefcopy@gmail.com>
2026-08-12 12:29:28 +0100

    ClaireLogger: out of core logger middleware

---
+++ src/middleware/ClaireLogger.ts
@@ -0,0 +1,8 @@
+import type { ClaireContext } from "../core/context";
+import { ClaireMiddleware } from "../core/middleware";
+
+export class ClaireLogger extends ClaireMiddleware {
+    override before(c: ClaireContext) {
+        console.log('Welcome to ClaireX!');
+    }
+}
