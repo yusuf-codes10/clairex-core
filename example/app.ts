de0c141ba454630b30c77cb7c5a06210139d4ea4
Yusef Codes <youcefcopy@gmail.com>
2026-08-13 17:44:13 +0100

    ClaireValidator setup: going with a class to implement first

--- example/app.ts
+++ example/app.ts
@@ -1,18 +1,8 @@
 import { ClaireX } from "../src";
 import { userController } from '../example/users.controller';
-import { logger } from "./logger";
-import { tester } from "./tester";
-import { global } from "./middlewares/global";
 
 const app = new ClaireX(2300);
 
-// need a way to mount the extends class here
 app.mount(new userController());
-
-app.use(new global());
-
-// middlwares
-// app.use(new logger());
-// app.use(new tester());
 
 app.listen();
---
+++ src/core/validator.ts
@@ -0,0 +1,5 @@
+export class ClaireValidator {
+    rules () {
+        
+    }
+}
