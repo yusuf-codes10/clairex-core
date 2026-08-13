ba2bba91765d5bcee1e951b3bb2a6e3151201724
Yusef Codes <youcefcopy@gmail.com>
2026-08-13 18:48:13 +0100

    looping throught the record and checking each rule

--- src/core/validator.ts
+++ src/core/validator.ts
@@ -1,17 +1,30 @@
+import type { ClaireContext } from "./context";
 import { ClaireMiddleware } from "./middleware";
 
 type ValidationRule = {
-    type: number | string | boolean,
-    required?: boolean,
-    min?: number,
-    max?: number
-}
+  type: number | string | boolean;
+  required?: boolean;
+  min?: number;
+  max?: number;
+};
 
 type ValidationSchema = Record<string, ValidationRule>;
 export abstract class ClaireValidator extends ClaireMiddleware {
-    abstract rules (): ValidationSchema;
+  abstract rules(): ValidationSchema;
 
-    override before (){
-        
+  override async before(c: ClaireContext) {
+
+    const body = await c.request.json();
+    const schema = this.rules();
+
+    // check body against this.rules()
+    // if invalid → throw ValidationException
+    // if valid → store typed data somewhere
+
+    for (const key in schema) {
+        const rule = schema[key];
+        const value = body[key]; // uknown
+
     }
+  }
 }
