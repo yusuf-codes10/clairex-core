f5e7a7f4127871bc4798cbe9f6457d4ed37e112a
Yusef Codes <youcefcopy@gmail.com>
2026-08-13 19:11:23 +0100

    ClaireException updated: addiing some exceptions

--- src/core/validator.ts
+++ src/core/validator.ts
@@ -1,4 +1,5 @@
 import type { ClaireContext } from "./context";
+import { ClaireException } from "./exception";
 import { ClaireMiddleware } from "./middleware";
 
 type ValidationRule = {
@@ -13,7 +14,7 @@ export abstract class ClaireValidator extends ClaireMiddleware {
   abstract rules(): ValidationSchema;
 
   override async before(c: ClaireContext) {
-    const body = await c.request.json() as Record<string, unknown>;
+    const body = (await c.request.json()) as Record<string, unknown>;
     const schema = this.rules();
 
     // check body against this.rules()
@@ -27,32 +28,52 @@ export abstract class ClaireValidator extends ClaireMiddleware {
       // 1. Required Check
       if (rule?.required && (value === undefined || value === null)) {
         // throw filed is missing
+        return new ClaireException(
+          400,
+          `Validation field: ${key} is required!`,
+        ).toResponse();
       }
 
       // 2. Type check
       if (value !== undefined && typeof value !== rule?.type) {
         // throw wrong type
+        return new ClaireException(
+          400,
+          `Validation failed! "${key}" must be of type "${rule?.type}"`,
+        ).toResponse();
       }
 
       // 3. min check
       if (rule && rule.min !== undefined && value !== undefined) {
         if (typeof value === "string" && value.length < rule.min) {
-          // throw
+          return new ClaireException(
+            400,
+            `Validation failed: "${key}" must be at least ${rule.min} characters`,
+          ).toResponse();
         }
 
         if (typeof value === "number" && value < rule.min) {
-          // throw
+          return new ClaireException(
+            400,
+            `Validation failed: "${key}" must be at least ${rule.min}`,
+          ).toResponse();
         }
       }
 
       // 4. max check
       if (rule && rule.max !== undefined && value !== undefined) {
-        if (typeof value === "string" && value.length < rule.max) {
-          // throw
+        if (typeof value === "string" && value.length > rule.max) {
+          return new ClaireException(
+            400,
+            `Validation failed: "${key}" must be at most ${rule.max} characters`,
+          ).toResponse();
         }
 
-        if (typeof value === "number" && value < rule.max) {
-          // throw
+        if (typeof value === "number" && value > rule.max) {
+          return new ClaireException(
+            400,
+            `Validation failed: "${key}" must be at most ${rule.max}`,
+          ).toResponse();
         }
       }
 
