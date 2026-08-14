83f86a627aa9c6d9fc01ee3d76b73facfbe7028c
Yusef Codes <youcefcopy@gmail.com>
2026-08-14 17:55:48 +0100

    ClaireContext updated: adding JSdocs

--- src/core/context.ts
+++ src/core/context.ts
@@ -2,22 +2,32 @@ import { ClaireRequest } from "./request";
 import { ClaireResponse } from "./response";
 
 export class ClaireContext {
-    // composition of both ClaireRouter & ClaireResponse here
-    public request: ClaireRequest;
-    public response: ClaireResponse;
+  // composition of both ClaireRouter & ClaireResponse here
+  public request: ClaireRequest;
+  public response: ClaireResponse;
 
-    private _valid: unknown = {};
+  private _valid: unknown = {};
 
-    constructor (req: Request) {
-        this.request = new ClaireRequest(req);
-        this.response = new ClaireResponse();
-    }
+  constructor(req: Request) {
+    this.request = new ClaireRequest(req);
+    this.response = new ClaireResponse();
+  }
 
-    set body(data: unknown) {
-        this._valid = data;
-    }
+  set body(data: unknown) {
+    this._valid = data;
+  }
 
-    valid<T>(): T {
-        return this._valid as T;
-    }
+  /**
+   * Returns the validated request body as a typed object.
+   * Must be used after a ClaireValidator middleware has run on the route.
+   *
+   * @template T - The expected type of the validated body.
+   * @returns The validated body cast to type T.
+   *
+   * @example
+   * const user = c.valid<User>();
+   */
+  valid<T>(): T {
+    return this._valid as T;
+  }
 }
