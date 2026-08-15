353b7e76f76c83a7d4e5fff685423a10e496a7b9
Yusef Codes <youcefcopy@gmail.com>
2026-08-15 19:35:52 +0100

    example: two issue fixed

--- example/app.ts
+++ example/app.ts
@@ -1,4 +1,5 @@
 import { ClaireX } from "../src";
 import { userKey } from "./keys/users.key";
+import { updateUserValidator } from "./validators/users/patchValidator";
 
 new ClaireX(2300).unlock(new userKey()).listen();
