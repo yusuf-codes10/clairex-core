2026ab2513cfb9ecc5dd16a579b090e278e679c6
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 12:59:14 +0100

    ClaireCors updated: returing the headers if the method is options

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,24 +1,41 @@
+import type { ClaireContext } from "../core/context";
 import { ClaireMiddleware } from "../core/middleware";
 
 export class ClaireCors extends ClaireMiddleware {
-    private _origin: string;
-    private _alloweHeaders: string[];
-    private _allowedMethods: string[];
-    private _exposeHeaders: string[];
+  private _origin: string;
+  private _alloweHeaders: string[];
+  private _allowedMethods: string[];
+  private _exposeHeaders: string[];
 
-    constructor (origin: string, allowedHeaders: string[], allowedMethods: string[], exposeHeaders: string[]) {
-        super();
-        this._origin = origin;
-        this._alloweHeaders = allowedHeaders;
-        this._allowedMethods = allowedMethods;
-        this._exposeHeaders = exposeHeaders
-    }
+  constructor(
+    origin: string,
+    allowedHeaders: string[],
+    allowedMethods: string[],
+    exposeHeaders: string[],
+  ) {
+    super();
+    this._origin = origin;
+    this._alloweHeaders = allowedHeaders;
+    this._allowedMethods = allowedMethods;
+    this._exposeHeaders = exposeHeaders;
+  }
 
-    override before() {
-        // returning options | short circuit
+  override before(c: ClaireContext) {
+    // returning options | short circuit
+    if (c.request.method === "OPTIONS") {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          "Access-Control-Allow-Origin": this._origin,
+          "Access-Control-Allow-Methods": this._allowedMethods.join(", "),
+          "Access-Control-Allow-Headers": this._alloweHeaders.join(", "),
+          "Access-Control-Expose-Headers": this._exposeHeaders.join(", "),
+        },
+      });
     }
+  }
 
-    override after() {
-        // add cors headers
-    }
+  override after() {
+    // add cors headers
+  }
 }
