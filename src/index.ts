fd5d00cde216afec069067d3d1ed03e49039165f
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 16:54:27 +0100

    barrel export updated

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-import { ClaireX } from "./core/clairex";
+// Core
+export { ClaireX } from './core/clairex';
+export { ClaireContext } from './core/context';
+export { ClaireKey } from './core/key';
+export { ClaireMiddleware } from './core/middleware';
+export { ClaireValidator } from './core/validator';
+export { ClaireException } from './core/exception';
+export { ClaireRequest } from './core/request';
+export { ClaireResponse } from './core/response';
 
-export { ClaireX};
+// Utils
+export { ClaireUtil } from './utils/util';
+
+// Middleware
+export { ClaireLogger } from './middleware/ClaireLogger';
+export { ClaireCors } from './middleware/cors';
+export { ClaireJWT } from './middleware/jwt';
+
+// Types
+export type { ClaireHandler, RouterEntry, ValidationSchema, ValidationRule } from './core/types';
