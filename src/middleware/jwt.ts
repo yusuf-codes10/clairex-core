51b5d87fe8929e3ada39973f29acead4555b9682
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 13:36:35 +0100

    ClaireJWT updated: 3 steps, needs somthing to geenrate and parse those JWT token

--- src/middleware/jwt.ts
+++ src/middleware/jwt.ts
@@ -1,8 +1,19 @@
 import type { ClaireContext } from "../core/context";
+import { ClaireException } from "../core/exception";
 import { ClaireMiddleware } from "../core/middleware";
 
 export class ClaireJWT extends ClaireMiddleware {
     override before(c: ClaireContext) {
+        const authHeader = c.request.headers.get('authorization');
 
+        // 1. Check if the header exists
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return new ClaireException(401, 'Missing or invalid token!').toResponse();
+        }
+
+        // 2. Extract token
+        const token = authHeader.split(' ')[1];
+
+        // 3. Verify token
     }
 }
