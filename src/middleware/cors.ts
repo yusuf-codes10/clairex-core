02afd1482c9625c2f858cb1d43e106bdc2783cc5
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 12:50:59 +0100

    ClaireCors updated: the skelteton, before to return the ptions, and after to set the headers

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,12 +4,21 @@ export class ClaireCors extends ClaireMiddleware {
     private _origin: string;
     private _alloweHeaders: string[];
     private _allowedMethods: string[];
+    private _exposeHeaders: string[];
 
-    constructor (origin: string, allowedHeaders: string[], allowedMethods: string[]) {
+    constructor (origin: string, allowedHeaders: string[], allowedMethods: string[], exposeHeaders: string[]) {
         super();
         this._origin = origin;
         this._alloweHeaders = allowedHeaders;
         this._allowedMethods = allowedMethods;
+        this._exposeHeaders = exposeHeaders
+    }
 
+    override before() {
+        // returning options | short circuit
+    }
+
+    override after() {
+        // add cors headers
     }
 }
