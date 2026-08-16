b33eb3839c2f80f4ff4a9005d52748b766bf5511
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 13:17:26 +0100

    ClaireMiddleware: setting up ClaireJWT

---
+++ src/middleware/jwt.ts
@@ -0,0 +1,8 @@
+import type { ClaireContext } from "../core/context";
+import { ClaireMiddleware } from "../core/middleware";
+
+export class ClaireJWT extends ClaireMiddleware {
+    override before(c: ClaireContext) {
+
+    }
+}
