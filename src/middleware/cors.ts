e9a74966aa1cec59669e944a082a3b64f70f4284
Yusef Codes <youcefcopy@gmail.com>
2026-08-16 12:45:13 +0100

    ClaireCors updated: adding some fields and a constuctor

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,5 +1,15 @@
 import { ClaireMiddleware } from "../core/middleware";
 
 export class ClaireCors extends ClaireMiddleware {
+    private _origin: string;
+    private _alloweHeaders: string[];
+    private _allowedMethods: string[];
 
+    constructor (origin: string, allowedHeaders: string[], allowedMethods: string[]) {
+        super();
+        this._origin = origin;
+        this._alloweHeaders = allowedHeaders;
+        this._allowedMethods = allowedMethods;
+
+    }
 }
