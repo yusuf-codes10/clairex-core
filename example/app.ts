22c090afbfba023fe10188d8c1635fe348aa5295
Yusef Codes <youcefcopy@gmail.com>
2026-08-17 19:52:44 +0100

    every .claire file is considered .ts file

--- example/app.ts
+++ example/app.ts
@@ -1,5 +1,5 @@
 import { ClaireX } from "../src";
-import { UserKey } from "./keys/test.claire";
+import { userKey } from "./keys/test.claire";
 import { updateUserValidator } from "./validators/users/patchValidator";
 
-new ClaireX(2300).unlock(new UserKey()).listen();
+new ClaireX(2300).unlock(new userKey()).listen();
