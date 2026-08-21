3cf2494b145dada46ba32e8f501cfe12a8f7e237
Yusef Codes <youcefcopy@gmail.com>
2026-08-21 18:39:14 +0100

    Example cleaned

--- example/app.ts
+++ example/app.ts
@@ -1,5 +1,4 @@
 import { ClaireX } from "../src";
 import { userKey } from "./keys/test.claire";
-import { hello } from './keys/posts.claire';
 
 new ClaireX(2300).unlock(new userKey()).listen();
--- example/logger.ts
+++
@@ -1,8 +0,0 @@
-import type { ClaireContext } from "../src/core/context";
-import { ClaireMiddleware } from "../src/core/middleware";
-
-export class logger extends ClaireMiddleware {
-    override before (c: ClaireContext) {
-        console.log('first middlware', c.request.method, c.request.pathname);
-    }
-}
